6f8563454cf4b7bc8c10fdb0258fedfd6a474ab9
franpfeiffer <franciscopfeiffer.business@gmail.com>
2024-12-02 20:05:23 +0000

    y ahora?

--- Formula/t2.rb
+++ Formula/t2.rb
@@ -1,7 +1,7 @@
 class T2 < Formula
   desc "TUI app made with go to track the time you spend on a project"
   homepage "https://github.com/franpfeiffer/t2"
-  url "https://github.com/franpfeiffer/t2.git", branch: "master"
+  url "https://github.com/franpfeiffer/t2.git", branch: "master" 
   license "MPL-2.0"
   head "https://github.com/franpfeiffer/t2.git", branch: "master"
 
