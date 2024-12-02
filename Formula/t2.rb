324cc1c599b1f314bd02aad9a7c90ece22bd2d55
franpfeiffer <franciscopfeiffer.business@gmail.com>
2024-12-02 19:26:20 +0000

    i think it works now

---
+++ Formula/t2.rb
@@ -0,0 +1,17 @@
+class T2 < Formula
+  desc "TUI app made with go to track the time you spend on a project"
+  homepage "https://github.com/franpfeiffer/t2"
+  url "https://github.com/franpfeiffer/t2.git", tag: "v0.1.0"
+  license "MPL-2.0"
+  head "https://github.com/franpfeiffer/t2.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "false"
+  end
+end
