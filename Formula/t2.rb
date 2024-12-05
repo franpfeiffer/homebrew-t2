c26896ff791fbc48e2d60991d466d1b9e924a4db
Francisco Pfeiffer <franciscopfeiffer.business@gmail.com>
2024-12-05 12:46:06 -0300

    uwu3

--- Formula/t2.rb
+++ Formula/t2.rb
@@ -1,7 +1,7 @@
 class T2 < Formula
   desc "TUI app made with go to track the time you spend on a project"
   homepage "https://github.com/franpfeiffer/t2"
-  url "https://github.com/franpfeiffer/t2.git", tag:"0.2.0"
+  url "https://github.com/franpfeiffer/t2.git", tag: "0.2.0"
   license "MPL-2.0"
   head "https://github.com/franpfeiffer/t2.git", branch: "master"
 
